2c96087ce7ae0540a6a1db75d4afb434a78bee5a
GKuzin13 <yo.is.josh@gmail.com>
2021-07-05 01:45:43 +0300

    Fixed board post return data

--- server/routes/api/boardRouter.js
+++ server/routes/api/boardRouter.js
@@ -3,6 +3,8 @@ const boardController = require('../../controllers/boardController');
 
 router.get('/b/:id', boardController.board_get);
 
+router.get('/:username/boards', boardController.board_all_get);
+
 router.post('/b/', boardController.create_board_post);
 
 router.patch('/b/:id/', boardController.update_board_patch);
