d512bdc71146ab609d3bad3a3eea64bfa29d875f
GKuzin13 <yo.is.josh@gmail.com>
2021-08-25 21:51:39 +0300

    Added mongodb memory server for testing

---
+++ server/utils/testDb.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = new MongoMemoryServer();
+
+// Connect to memory db
+module.exports.connect = async () => {
+  const uri = await mongod.getUri();
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    poolSize: 10,
+  };
+
+  await mongoose.connect(uri, mongooseOpts);
+};
+
+// Disconnect and close db connection
+module.exports.closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
+
+// Clear the db and remove all data
+module.exports.clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
