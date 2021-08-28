2d4b36529923ef282b95399a96ffb27b661833e9
GKuzin13 <yo.is.josh@gmail.com>
2021-08-28 21:36:20 +0300

    Added more items to demo board

--- utils/populateBoard.js
+++ utils/populateBoard.js
@@ -1,10 +1,12 @@
 const Board = require('../models/board');
 const List = require('../models/list');
 const Card = require('../models/card');
-const mongoose = require('mongoose');
+const Subtask = require('../models/subtask');
+
+const { Types } = require('mongoose');
 
 exports.populateGuestBoard = async (userId) => {
-  const boardId = mongoose.Types.ObjectId().toHexString();
+  const boardId = Types.ObjectId().toHexString();
 
   const guestBoard = {
     _id: boardId,
@@ -14,28 +16,28 @@ exports.populateGuestBoard = async (userId) => {
 
   const guestLists = [
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       listTitle: 'Backlog',
       position: 16384,
       coverColor: 'gray',
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       listTitle: 'Planned',
       position: 32768,
       coverColor: 'blue',
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       listTitle: 'In Progress',
       position: 49152,
       coverColor: 'purple',
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       listTitle: 'Completed',
       position: 65536,
       coverColor: 'green',
@@ -45,16 +47,16 @@ exports.populateGuestBoard = async (userId) => {
 
   const guestCards = [
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
-      cardTitle: 'Welcome to Company',
-      description: 'Company is a bla bla',
+      _id: Types.ObjectId().toHexString(),
+      cardTitle: 'Welcome to Jella',
+      description: 'Jella is a powerful tool to visually manage your workflow.',
       priority: 'low',
       position: 16384,
       listId: guestLists[0]._id,
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       cardTitle: 'You can add lists',
       description:
         'A list is a collection of vertically-arranged cards. They may represent a collection of ideas, things to remember, or different stages of a workflow.',
@@ -64,26 +66,27 @@ exports.populateGuestBoard = async (userId) => {
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
-      cardTitle: 'And add detailed description to cards',
-      description: 'Just like this!',
-      priority: 'low',
+      _id: Types.ObjectId().toHexString(),
+      cardTitle: 'And add a detailed description to cards',
+      description: '',
+      priority: 'medium',
       position: 49152,
       listId: guestLists[0]._id,
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
-      cardTitle: 'Break big tasks into smaller actionable steps',
+      _id: Types.ObjectId().toHexString(),
+      cardTitle:
+        'Break big tasks into smaller actionable steps using a checklist',
       description:
         'A checklist is a way of keeping track of subtasks within a card.',
-      priority: 'medium',
+      priority: 'high',
       position: 16384,
       listId: guestLists[1]._id,
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       cardTitle: 'Try to rearrange lists and cards',
       description: '',
       priority: 'medium',
@@ -92,7 +95,7 @@ exports.populateGuestBoard = async (userId) => {
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       cardTitle: 'You can set three diffrent levels of priority',
       description: '',
       priority: 'high',
@@ -101,21 +104,101 @@ exports.populateGuestBoard = async (userId) => {
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
-      cardTitle: 'And add detailed description to cards',
-      description: 'Just like this!',
+      _id: Types.ObjectId().toHexString(),
+      cardTitle: 'And change the list cover color',
+      description: '',
       priority: 'low',
       position: 49152,
       listId: guestLists[2]._id,
       boardId: boardId,
     },
     {
-      _id: mongoose.Types.ObjectId().toHexString(),
+      _id: Types.ObjectId().toHexString(),
       cardTitle: 'How do you like it?',
       description: '',
       priority: 'low',
       position: 16384,
       listId: guestLists[3]._id,
+      boardId: boardId,
+    },
+  ];
+
+  const guestSubtasks = [
+    {
+      taskName: 'Add a new list',
+      isDone: false,
+      position: 16384,
+      cardId: guestCards[1]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Add a description to this card',
+      isDone: false,
+      position: 16384,
+      cardId: guestCards[2]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Check this subtask',
+      isDone: false,
+      position: 16384,
+      cardId: guestCards[3]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Already checked',
+      isDone: true,
+      position: 32768,
+      cardId: guestCards[3]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Rearrange lists',
+      isDone: false,
+      position: 16384,
+      cardId: guestCards[4]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Rearrange cards between different lists',
+      isDone: false,
+      position: 32768,
+      cardId: guestCards[4]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Change the priority for this card',
+      isDone: false,
+      position: 16384,
+      cardId: guestCards[5]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Change the cover color of this list',
+      isDone: false,
+      position: 32768,
+      cardId: guestCards[6]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Some task',
+      isDone: true,
+      position: 16384,
+      cardId: guestCards[7]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Some other task',
+      isDone: true,
+      position: 32768,
+      cardId: guestCards[7]._id,
+      boardId: boardId,
+    },
+    {
+      taskName: 'Another task',
+      isDone: true,
+      position: 49152,
+      cardId: guestCards[7]._id,
       boardId: boardId,
     },
   ];
@@ -126,7 +209,9 @@ exports.populateGuestBoard = async (userId) => {
     await List.insertMany(guestLists);
 
     await Card.insertMany(guestCards);
+
+    await Subtask.insertMany(guestSubtasks);
   } catch (error) {
-    return error;
+    console.log(error);
   }
 };
