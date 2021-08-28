123ae80ee9cc8c0dbd3b78e20b72a22f82b3dfbc
GKuzin13 <yo.is.josh@gmail.com>
2021-08-28 21:36:41 +0300

    Changed tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,9 +14,9 @@ module.exports = {
       backgroundImage: (theme) => ({
         main: 'linear-gradient( 75.2deg, rgb(25, 87 ,232 , 0.15) -2.5%, rgb(222, 100, 29 , 0.025) 55%, rgb(0 ,90 ,255 , 0.05) 102.3% )',
         boardPage:
-          'linear-gradient( 75.2deg, rgb(137,154,193, 50%) -2.5%, rgb(107,120,150, 30%) 55%, rgb(131,142,173, 50%) 102.3% )',
-        board:
-          'radial-gradient( circle farthest-corner at 7.5% 24%,  rgba(237,161,193,0.075) 0%, rgba(250,178,172,0.075) 25.5%, rgba(190,228,210,0.075) 62.3%, rgba(215,248,247,0.075) 93.8% )',
+          'linear-gradient( 73deg, rgb(145, 155, 177, 55%) -2.5%, rgb(66, 77, 103, 40%) 55%, rgb(138,149,167, 75%) 102.3% )',
+        homePage:
+          'linear-gradient( 75.2deg, rgb(25, 87 ,232 , 0.15) -2.5%, rgb(148, 144, 126, 12%) 55%, rgb(0 ,90 ,255 , 0.05) 102.3% )',
       }),
       zIndex: {
         '-10': '-10',
