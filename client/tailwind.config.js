358c56fb6107503bd0ced1aa4615f43e9a1ef9b9
GKuzin13 <yo.is.josh@gmail.com>
2021-08-28 13:07:29 +0300

    Changed tailwind purge config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
