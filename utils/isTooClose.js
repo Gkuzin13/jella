ecbb167cc1c5f5e830fd909aad3c55b8b18b80fc
GKuzin13 <yo.is.josh@gmail.com>
2021-08-31 00:49:07 +0300

    Added comments

--- utils/isTooClose.js
+++ utils/isTooClose.js
@@ -1,5 +1,6 @@
-// Checks if item position is
+// Checks if item position is too close to its neighbouring items
 const isTooClose = (position) => {
+  // Checks if number is decimal and if its remainder is less than 0.01
   if (!Number.isInteger(position) && position % 1 < 0.01) {
     return true;
   }
