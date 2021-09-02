80f8fde60048e8eac62f86c3448ffd996c658ed6
GKuzin13 <yo.is.josh@gmail.com>
2021-09-02 23:51:04 +0300

    Fixed padding issue

--- client/src/components/CardDetailsBox/Subtask.js
+++ client/src/components/CardDetailsBox/Subtask.js
@@ -26,7 +26,7 @@ const SubTask = ({
           className={`flex justify-between bg-white hover:bg-gray-50 items-center
           w-full bg-opacity-95 py-2 px-1 ${draggingStyle}`}
           key={subtask._id}>
-          <div className='flex items-center '>
+          <div className='flex items-center'>
             <input
               id='isDone'
               type='checkbox'
@@ -43,12 +43,12 @@ const SubTask = ({
             </label>
           </div>
 
-          <button
-            type='button'
+          <span
+            role='button'
             onClick={() => onSubtaskDelete()}
-            className='flex items-center text-gray-300'>
-            <span className='material-icons hover:text-red-500'>clear</span>
-          </button>
+            className='material-icons text-gray-400 hover:text-red-500'>
+            clear
+          </span>
         </div>
       )}
     </Draggable>
