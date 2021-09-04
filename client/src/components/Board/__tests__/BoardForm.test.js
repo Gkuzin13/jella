ced81f919ae08a29037512017b5f0358ab9d661e
GKuzin13 <yo.is.josh@gmail.com>
2021-09-04 23:12:54 +0300

    Removed an unused imported function

--- client/src/components/Board/__tests__/BoardForm.test.js
+++ client/src/components/Board/__tests__/BoardForm.test.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import BoardForm from '../BoardForm';
 
