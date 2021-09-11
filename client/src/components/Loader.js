1eee537dbcb6628a94f24881b5306e0e66075d71
GKuzin13 <yo.is.josh@gmail.com>
2021-09-11 13:11:05 +0300

    Changed main loader animation

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,9 +2,9 @@ const Loader = () => {
   return (
     <div className='grid place-items-center h-screen w-full'>
       <div className='flex items-start justify-start'>
-        <span className='h-10 w-3.5 mx-0.5 bg-gray-400 bg-opacity-80 animate-stretch rounded-sm shadow-inner'></span>
-        <span className='h-10 w-3.5 mx-0.5 bg-gray-400 bg-opacity-70 animate-stretch2 rounded-sm shadow-inner'></span>
-        <span className='h-10 w-3.5 mx-0.5 bg-gray-300 animate-stretch3 rounded-sm shadow-inner'></span>
+        <span className='h-8 w-3 opacity-0 bg-gray-300 animate-scale transform-gpu transition-transform rounded-sm'></span>
+        <span className='h-8 w-3 mx-1 opacity-0 bg-gray-400 bg-opacity-70 animate-scale2 transform-gpu transition-transform rounded-sm'></span>
+        <span className='h-8 w-3 opacity-0 bg-gray-400 bg-opacity-80 animate-scale3 transform-gpu transition-transform rounded-sm'></span>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -25,39 +25,36 @@ module.exports = {
         blue: '2px solid rgb(37, 99, 235)',
       },
       animation: {
-        stretch: 'stretch 1.2s ease-in-out infinite',
-        stretch2: 'stretch2 1.1s ease-in-out infinite',
-        stretch3: 'stretch3 1s ease-in-out infinite',
+        scale: 'scale 1s infinite',
+        scale2: 'scale2 1s 0.2s infinite',
+        scale3: 'scale3 1s 0.4s infinite',
       },
       keyframes: {
-        stretch: {
+        scale: {
           '0%, 100%': {
-            transform: 'scaleY(1)',
-            opacity: '0.8',
+            transform: 'scale(0)',
           },
           '50%': {
-            transform: 'scaleY(1.15)',
-            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+            opacity: '1',
+            transform: 'scale(1)',
           },
         },
-        stretch2: {
+        scale2: {
           '0%, 100%': {
-            transform: 'scaleY(1)',
-            opacity: '0.7',
-            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+            transform: 'scale(0)',
           },
           '50%': {
-            transform: 'scaleY(1.2)',
+            opacity: '1',
+            transform: 'scale(1)',
           },
         },
-        stretch3: {
+        scale3: {
           '0%, 100%': {
-            transform: 'scaleY(1)',
-            opacity: '0.9',
-            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+            transform: 'scale(0)',
           },
           '50%': {
-            transform: 'scaleY(1.3)',
+            opacity: '1',
+            transform: 'scale(1)',
           },
         },
       },
