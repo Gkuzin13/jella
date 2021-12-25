d2f14f4370c7adaf46b0be5b3a993a2259a28062
GKuzin13 <yo.is.josh@gmail.com>
2021-12-25 22:29:33 +0200

    updated Home page route link

--- client/src/components/Board/BoardNav.js
+++ client/src/components/Board/BoardNav.js
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom';
 import UserControl from '../UserControl';
 import boardApi from '../../api/boardApi';
 import EditableText from '../EditableText';
+import { useContext } from 'react';
+import { AuthContext } from '../../config/Auth';
 
-const BoardNav = ({ boardData, user, setBoardData }) => {
+const BoardNav = ({ boardData, setBoardData }) => {
+  const { user } = useContext(AuthContext);
+
   const handleBoardUpdate = async (updatedTitle) => {
     const updatedBoard = { ...boardData, title: updatedTitle };
     setBoardData(() => updatedBoard);
@@ -23,23 +27,20 @@ const BoardNav = ({ boardData, user, setBoardData }) => {
     <div className='bg-white bg-opacity-90 flex justify-between items-center py-3 px-8 mb-4 shadow'>
       <div className='flex items-center text-xl font-bold'>
         <Link
-          to={`/${user.username}/boards`}
+          to={`/${user.username}`}
           aria-label='Back to home'
           className='flex items-center opacity-90 px-1 py-0.5 mr-0.5 text-gray-500 hover:text-gray-600 transition-colors 
           duration-100'>
           <span className='material-icons-outlined mr-1'>home</span>
           Boards
         </Link>
-
         <span className='text-gray-500'> / </span>
-
         <EditableText
           style={titleStyle.style}
           dataText={boardData.title}
           dataUpdateFunc={handleBoardUpdate}
         />
       </div>
-
       <UserControl />
     </div>
   );
