b5a46d2b1b9004e39b02572771489d18cc80fef3
GKuzin13 <yo.is.josh@gmail.com>
2021-12-25 22:27:26 +0200

    changed BrowserRouter to its default import name

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './config/Auth';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <Router>
+      <BrowserRouter>
         <App />
-      </Router>
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
