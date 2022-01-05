9281c80ed6f7b0233dc937cde816302a3c813529
GKuzin13 <yo.is.josh@gmail.com>
2022-01-05 19:38:34 +0200

    fixed spacing and margin issues when dragging cards

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,53 +6,48 @@ const Card = ({ cardData, toggleCardBox, index }) => {
   const { total, done } = calcTasksStats(cardData.subtasks || []);
   const priorityIcon = getPriorityIcon(cardData.priority);
   const progressColor = getProgressColor(total, done);
-
   const priorityColor = `text-${priorityIcon.color}-500`;
 
   return (
     <Draggable draggableId={cardData._id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className='bg-white shadow-md cursor-pointer rounded-sm hover:shadow-lg
-          transition-shadow duration-150 mb-3'>
-          <div
-            onClick={() => toggleCardBox(cardData._id, true)}
-            className='flex flex-col w-full'>
-            <span className='font-medium text-gray-900 break-words px-3 pt-2.5'>
-              {cardData.cardTitle}
-            </span>
-            <div className='flex items-center justify-start w-full mt-2'>
-              {total === 0 ? null : (
-                <div
-                  title='Checklist items'
-                  className={`${progressColor} flex items-center justify-center rounded px-1.5 ml-3 mb-2.5 `}>
-                  <span className='material-icons-outlined text-lg pl-0 pr-0.5 py-0'>
-                    check_box
-                  </span>
-                  <div className='p-0.5 pt-1 text-sm'>
-                    <span>{done}/</span>
-                    <span>{total}</span>
-                  </div>
-                </div>
-              )}
-
-              {cardData.description && (
-                <span
-                  title='This card has a description.'
-                  className='material-icons-outlined text-gray-300 mb-2.5 ml-3'>
-                  subject
+          className={`bg-white cursor-pointer rounded-sm hover:shadow-lg
+          transition-shadow duration-150 py-2 px-2.5 mb-3 ${
+            snapshot.isDragging ? 'shadow-lg' : 'shadow'
+          }`}
+          onClick={() => toggleCardBox(cardData._id, true)}>
+          <span className='font-medium text-gray-900 pb-2 block'>
+            {cardData.cardTitle}
+          </span>
+          <div className='flex items-start mt-0.5'>
+            {total === 0 ? null : (
+              <div
+                title='Checklist items'
+                className={`${progressColor} flex items-center justify-center rounded px-1.5 mr-3`}>
+                <span className='material-icons-outlined text-xl'>
+                  check_box
                 </span>
-              )}
-
+                <span className='ml-1 text-sm'>
+                  {done}/{total}
+                </span>
+              </div>
+            )}
+            {cardData.description && (
               <span
-                title='Card priority'
-                className={`material-icons mb-2.5 mr-3 ml-auto ${priorityColor}`}>
-                {priorityIcon.icon}
+                title='This card has a description.'
+                className='material-icons-outlined text-gray-300'>
+                subject
               </span>
-            </div>
+            )}
+            <span
+              title='Card priority'
+              className={`material-icons mb-2.5 mr-1 ml-auto ${priorityColor}`}>
+              {priorityIcon.icon}
+            </span>
           </div>
         </div>
       )}
