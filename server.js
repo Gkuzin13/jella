0dc69dc5e65166df9c8ca88bc8c585239dae1499
GKuzin13 <yo.is.josh@gmail.com>
2022-10-30 23:42:01 +0200

    update session config

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const passport = require('passport');
-const session = require('express-session');
-const path = require('path');
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
-}
+// if (process.env.NODE_ENV === "development") {
+require("dotenv").config();
+// }
 
 // Use passport config
-const initialize = require('./config/passportConfig');
+const initialize = require("./config/passportConfig");
 initialize(passport);
 
-const homeRouter = require('./routes/api/homeRouter');
-const boardRouter = require('./routes/api/boardRouter');
-const listRouter = require('./routes/api/listRouter');
-const cardRouter = require('./routes/api/cardRouter');
+const homeRouter = require("./routes/api/homeRouter");
+const boardRouter = require("./routes/api/boardRouter");
+const listRouter = require("./routes/api/listRouter");
+const cardRouter = require("./routes/api/cardRouter");
 
 const app = express();
 
@@ -33,33 +32,21 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: false,
   },
   store: MongoStore.create({
     mongoUrl: process.env.MONGODB_URI,
-    dbName: 'sessions',
+    dbName: "sessions",
   }),
 };
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/api/auth', homeRouter);
-app.use('/api', boardRouter);
-app.use('/api', listRouter);
-app.use('/api', cardRouter);
-
-if (process.env.NODE_ENV === 'production') {
-  app.set('trust proxy', 1); // trust first proxy
-
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.use("/api/auth", homeRouter);
+app.use("/api", boardRouter);
+app.use("/api", listRouter);
+app.use("/api", cardRouter);
 
 app.listen(port, () => {
   // Get db connection
