970eaeed209003e22120cc4ca0aa68ab761d3064
GKuzin13 <yo.is.josh@gmail.com>
2022-11-05 17:38:23 +0200

    restore server render config

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://jella.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
   credentials: "include",
 });
--- server.js
+++ server.js
@@ -22,18 +22,25 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(helmet());
-app.use(cors({ credentials: true, origin: "https://jella-app.onrender.com" }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const sessionConfig = {
+  resave: false,
+  saveUninitialized: false,
   secret: process.env.SESSION_SECRET,
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  },
   store: MongoStore.create({
     mongoUrl: process.env.MONGODB_URI,
     dbName: "sessions",
   }),
 };
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(cors({ credentials: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,6 +49,17 @@ app.use("/api/auth", homeRouter);
 app.use("/api", boardRouter);
 app.use("/api", listRouter);
 app.use("/api", cardRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   // Get db connection
